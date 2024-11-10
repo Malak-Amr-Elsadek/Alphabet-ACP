a0ece28548eac565ca228ecf265c04f5733264e0
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-11-10 19:36:37 +0300

    Alphabet ACP

---
+++ main.py
@@ -0,0 +1,54 @@
+letter=int(input("Enter one letter:"))
+
+if letter=A  or  a:
+    print("Apple")
+elif letter=B  or  b:
+    print("Ball")
+elif letter=C  or  c:
+    print("Carrot")
+elif letter=D  or  d:
+    print("Dad")
+elif letter=E  or  e:
+    print("Egg")
+elif letter=F  or  f:
+    print("Fuel")
+elif letter=G  or  g:
+    print("Good")
+elif letter=H  or  h:
+    print("House")
+elif letter=I  or  i:
+    print("Idol")
+elif letter=J  or  j:
+    print("Jelly")
+elif letter=K  or  k:
+    print("Kid")
+elif letter=L  or  l:
+    print("Loop")
+elif letter=M  or  m:
+    print("Mum")
+elif letter=N  or  n:
+    print("Nice")
+elif letter=O  or  o:
+    print("Ostrich")
+elif letter=P  or  p:
+    print("Pass")
+elif letter=Q  or  q:
+    print("Queen")
+elif letter=R  or  r:
+    print("Rat  ")
+elif letter=S  or  s:
+    print("Snake")
+elif letter=T  or  t:
+    print("T-shirt")
+elif letter=U or  u:
+    print("Upset")
+elif letter=V  or  v:
+    print("Vibrant")
+elif letter=W  or  w:
+    print("Weak")
+elif letter=X  or  x:
+    print("Xtreme")
+elif letter=Y  or  y:
+    print("Yogurt")
+elif letter=Z  or  z:
+    print("Zebra")
